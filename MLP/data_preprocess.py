d8cc4725a7f9e05b3218ea08b6d5fb164a9936ed
Ding Zijian <dingzijian881218@126.com>
2017-12-18 17:45:10 +0800

    read_all_samples

--- MLP/data_preprocess.py
+++ MLP/data_preprocess.py
@@ -12,7 +12,14 @@ def read_signal(file_path, record_name):
  
 
 def read_all_signal(file_path, records):
-    
+    n_sample = records.shape[0]
+    samples = {}
+    for i in range(n_sample):
+        name = records[0][i]
+        signal, hea = read_signal(file_path, name)
+        samples[name + 'signal'] = signal
+        samples[name + 'header'] = hea
+    return samples
  
 
 if __name__ = 'main':
@@ -21,4 +28,4 @@ if __name__ = 'main':
      records = pandas.read_table(path + '/training2017/RECORDS', header=None) #records[0][0], [0][1], a col
      labels = pandas.read_csv(path + '/REFERENCE-v3.csv', header=None) # labels[0][0] and [1][0], a row
      
-     
+     samples = read_all_signal(path + '/training2017', records)
